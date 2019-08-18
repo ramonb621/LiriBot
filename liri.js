c2347e746e53a5365509e3f993bd775ae92c296f
Ramon Barajas <ramonb621@yahoo.com>
2019-08-17 20:00:43 -0700

    added concert search function that works

--- liri.js
+++ liri.js
@@ -4,13 +4,17 @@ var keys = require("./keys.js");
 
 var axios = require("axios");
 
+var moment = require('moment');
+
 var Spotify = require('node-spotify-api');
 
 var spotify = new Spotify(keys.spotify);
 
 var input = process.argv.slice(2);
+
 var params = input;
-// var song = tracks.href.items;
+
+
 
 switch(params[0]){
 
@@ -60,31 +64,49 @@ function spotifySearch(){
 }
 
 function concertSearch(){
-axios.get("https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp").then(
+axios.get("https://rest.bandsintown.com/artists/" + params[1] + "/events?app_id=codingbootcamp").then(
   function(response) {
-    // var band = response;
-    // for (var i = 0; i < band.length; i++) {
-    //   console.log("Artist/Band: " + band[i]);
-    //   console.log("----------------------------------------------------");
-    // }
-    console.log(input);
-    console.log(response);
+    var band = [
+      response.data[0],
+      response.data[1],
+      response.data[2],
+      response.data[3],
+      response.data[4],
+    ]
+  
+        console.log('Lineup: ' + band[0].lineup + '\nVenue Name: ' + band[0].venue.name +'\nCountry: ' + band[0].venue.country + '\nRegion: ' + band[0].venue.region + 
+        '\nCity: ' +  band[0].venue.city);
+        console.log('Date: ' + moment(band[0].datetime).format("MMMM Do YYYY, h:mm:ss a"));
+
+        console.log("---------------------------");
+
+        console.log('Lineup: ' + band[1].lineup + '\nVenue Name: ' + band[1].venue.name + '\nCountry: ' + band[1].venue.country + '\nRegion: ' + band[1].venue.region + 
+        '\nCity: ' +  band[1].venue.city);
+        console.log('Date: ' + moment(band[1].datetime).format("MMMM Do YYYY, h:mm:ss a"));
+
+        console.log("---------------------------");
+
+        console.log('Lineup: ' + band[2].lineup + '\nVenue Name: ' + band[2].venue.name +'\nCountry: ' + band[2].venue.country + '\nRegion: ' + band[2].venue.region + 
+        '\nCity: ' +  band[2].venue.city);
+        console.log('Date: ' + moment(band[2].datetime).format("MMMM Do YYYY, h:mm:ss a"));
+      
+        console.log("---------------------------");
+
+        console.log('Lineup: ' + band[3].lineup + '\nVenue Name: ' + band[3].venue.name + '\nCountry: ' + band[3].venue.country + '\nRegion: ' + band[3].venue.region + 
+        '\nCity: ' +  band[3].venue.city);
+        console.log('Date: ' + moment(band[3].datetime).format("MMMM Do YYYY, h:mm:ss a"));
+      
+        console.log("---------------------------");
+
+        console.log('Lineup: ' + band[4].lineup + '\nVenue Name: ' + band[4].venue.name + '\nCountry: ' + band[4].venue.country + '\nRegion: ' + band[4].venue.region + 
+        '\nCity: ' +  band[4].venue.city);
+        console.log('Date: ' + moment(band[4].datetime).format("MMMM Do YYYY, h:mm:ss a"));
+
   })
     .catch(function(error) {
-      // if(error.response){
-      //   console.log(error);
-      //   console.log("---------------Data---------------");
-      //   console.log(error.response.data);
-      //   console.log("---------------Status---------------");
-      //   console.log(error.response.status);
-      //   console.log("---------------Status---------------");
-      //   console.log(error.response.headers);
-      // } else if (error.request) {
-      //   console.log(error.request);
-      // } else {
-      //   console.log("Error", error.message);
-      // }
-      console.log(error);
+
+      console.error(error);
+      
     });  
 }
 
