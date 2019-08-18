369a5252211cb8f2d8558dd39902fcc36432876d
Ramon Barajas <ramonb621@yahoo.com>
2019-08-17 20:43:37 -0700

    restructured concert-this function

--- liri.js
+++ liri.js
@@ -66,54 +66,30 @@ function spotifySearch(){
 function concertSearch(){
 axios.get("https://rest.bandsintown.com/artists/" + params[1] + "/events?app_id=codingbootcamp").then(
   function(response) {
-    var band = [
-      response.data[0],
-      response.data[1],
-      response.data[2],
-      response.data[3],
-      response.data[4],
-    ]
-  
-        console.log('Lineup: ' + band[0].lineup + '\nVenue Name: ' + band[0].venue.name +'\nCountry: ' + band[0].venue.country + '\nRegion: ' + band[0].venue.region + 
-        '\nCity: ' +  band[0].venue.city);
-        console.log('Date: ' + moment(band[0].datetime).format("MMMM Do YYYY, h:mm:ss a"));
+    var band = response.data;
+    for (var i = 0; i < band.length; i++){
+
+        console.log('Lineup: ' + band[i].lineup + '\nVenue Name: ' + band[i].venue.name +'\nCountry: ' + band[i].venue.country + '\nRegion: ' + band[i].venue.region + 
+        '\nCity: ' +  band[i].venue.city);
+        console.log('Date: ' + moment(band[i].datetime).format("MMMM Do YYYY, h:mm:ss a"));
 
         console.log("---------------------------");
-
-        console.log('Lineup: ' + band[1].lineup + '\nVenue Name: ' + band[1].venue.name + '\nCountry: ' + band[1].venue.country + '\nRegion: ' + band[1].venue.region + 
-        '\nCity: ' +  band[1].venue.city);
-        console.log('Date: ' + moment(band[1].datetime).format("MMMM Do YYYY, h:mm:ss a"));
-
-        console.log("---------------------------");
-
-        console.log('Lineup: ' + band[2].lineup + '\nVenue Name: ' + band[2].venue.name +'\nCountry: ' + band[2].venue.country + '\nRegion: ' + band[2].venue.region + 
-        '\nCity: ' +  band[2].venue.city);
-        console.log('Date: ' + moment(band[2].datetime).format("MMMM Do YYYY, h:mm:ss a"));
-      
-        console.log("---------------------------");
-
-        console.log('Lineup: ' + band[3].lineup + '\nVenue Name: ' + band[3].venue.name + '\nCountry: ' + band[3].venue.country + '\nRegion: ' + band[3].venue.region + 
-        '\nCity: ' +  band[3].venue.city);
-        console.log('Date: ' + moment(band[3].datetime).format("MMMM Do YYYY, h:mm:ss a"));
-      
-        console.log("---------------------------");
-
-        console.log('Lineup: ' + band[4].lineup + '\nVenue Name: ' + band[4].venue.name + '\nCountry: ' + band[4].venue.country + '\nRegion: ' + band[4].venue.region + 
-        '\nCity: ' +  band[4].venue.city);
-        console.log('Date: ' + moment(band[4].datetime).format("MMMM Do YYYY, h:mm:ss a"));
+  }
 
   })
     .catch(function(error) {
 
       console.error(error);
-      
+
     });  
 }
 
 function movieSearch(){
   axios.get("http://www.omdbapi.com/?apikey=trilogy&type=movie&t=" + input + "").then(
   function(response) {
-    console.log(response);
+    // var movie = response.data;
+    // console.log('Title: ' + movie.Title);
+    console.log(response.Title);
   })
     .catch(function(error) {
       // if(error.response){
